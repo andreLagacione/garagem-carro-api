421fa5603b8434b0475158f8f7b260b751ddb79c
André Lagacione <lagacioneandre@gmail.com>
2019-04-21 19:11:49 -0300

    continuacao da classe EstadoResource

--- src/main/java/com/andrelagacione/garagemcarroapi/resources/EstadoResource.java
+++ src/main/java/com/andrelagacione/garagemcarroapi/resources/EstadoResource.java
@@ -1,21 +1,26 @@
 package com.andrelagacione.garagemcarroapi.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.andrelagacione.garagemcarroapi.domain.Cidade;
 import com.andrelagacione.garagemcarroapi.domain.Estado;
-import com.andrelagacione.garagemcarroapi.dto.CidadeDTO;
 import com.andrelagacione.garagemcarroapi.dto.EstadoDTO;
-import com.andrelagacione.garagemcarroapi.services.CidadeService;
 import com.andrelagacione.garagemcarroapi.services.EstadoService;
+import com.andrelagacione.garagemcarroapi.services.exceptions.ObjectNotFoundException;
 
 @RestController
 @RequestMapping(value="/estados")
@@ -24,20 +29,37 @@ public class EstadoResource {
 	@Autowired
 	private EstadoService estadoService;
 	
-	@Autowired
-	private CidadeService cidadeService;
-	
-	@RequestMapping(method=RequestMethod.GET)
+	@RequestMapping(value="/lista", method=RequestMethod.GET)
 	public ResponseEntity<List<EstadoDTO>> findAll() {
 		List<Estado> list = estadoService.findAll();
 		List<EstadoDTO> listDto = list.stream().map(obj -> new EstadoDTO(obj)).collect(Collectors.toList());  
 		return ResponseEntity.ok().body(listDto);
 	}
 	
-	@RequestMapping(value="/{estadoId}/cidades", method=RequestMethod.GET)
-	public ResponseEntity<List<CidadeDTO>> findCidades(@PathVariable Integer estadoId) {
-		List<Cidade> list = cidadeService.findByEstado(estadoId);
-		List<CidadeDTO> listDto = list.stream().map(obj -> new CidadeDTO(obj)).collect(Collectors.toList());  
-		return ResponseEntity.ok().body(listDto);
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<Page<EstadoDTO>> findPage(
+		@RequestParam(value="page", defaultValue="0") Integer page,
+		@RequestParam(value="size", defaultValue="25") Integer size,
+		@RequestParam(value="orderBy", defaultValue="nome") String orderBy,
+		@RequestParam(value="direction", defaultValue="ASC") String direction
+	) {
+		Page<Estado> estados = estadoService.findPage(page, size, orderBy, direction);
+		Page<EstadoDTO> estadoDTO = estados.map(obj -> new EstadoDTO(obj));
+		return ResponseEntity.ok().body(estadoDTO);
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Estado> find(@PathVariable Integer id) throws ObjectNotFoundException {
+		Estado estado = estadoService.find(id);
+		return ResponseEntity.ok().body(estado);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody EstadoDTO estadoDTO) {
+		Estado estado = estadoService.fromDto(estadoDTO);
+		estado = estadoService.insert(estado);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+					.path("/{id}").buildAndExpand(estado.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
