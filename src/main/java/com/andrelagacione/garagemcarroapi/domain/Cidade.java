4714edb6e20d249e81aa5ea76cce3260a7144e9c
André Lagacione <lagacioneandre@gmail.com>
2019-10-08 14:12:25 -0300

    .

--- src/main/java/com/andrelagacione/garagemcarroapi/domain/Cidade.java
+++ src/main/java/com/andrelagacione/garagemcarroapi/domain/Cidade.java
@@ -2,20 +2,18 @@ package com.andrelagacione.garagemcarroapi.domain;
 
 import java.io.Serializable;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "cidade")
 public class Cidade implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+
+	@Column(name = "value")
 	private String nome;
 	
 	@ManyToOne
