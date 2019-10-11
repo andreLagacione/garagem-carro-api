23939ed83f241972095ae020e6425558a89dbab4
andreLagacione <lagacioneandre@gmail.com>
2019-10-10 22:03:32 -0300

    remove hashcode e equals

--- src/main/java/com/andrelagacione/garagemcarroapi/domain/Categoria.java
+++ src/main/java/com/andrelagacione/garagemcarroapi/domain/Categoria.java
@@ -39,26 +39,4 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Categoria other = (Categoria) obj;
-		if (id != other.id)
-			return false;
-		return true;
-	}
 }
--- src/main/java/com/andrelagacione/garagemcarroapi/domain/Menu.java
+++ src/main/java/com/andrelagacione/garagemcarroapi/domain/Menu.java
@@ -63,29 +63,4 @@ public class Menu implements Serializable {
 	public void setIcone(String icone) {
 		this.icone = icone;
 	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Menu other = (Menu) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
 }
